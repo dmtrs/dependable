0a75edda7dca7f1f522f679c32ad032acc226f5b
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2021-03-31 00:06:57 +0300

    docs(test_examples): Enhance test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,6 +1,6 @@
 import asyncio
 import pytest
-from typing import Dict
+from typing import Dict, Tuple
 from collections import defaultdict
 
 from dependable import dependant, Depends
@@ -35,4 +35,37 @@ async def test_awaitable() -> None:
 
     assert await bar() is not await bar() is not await f()
 
+
+@pytest.mark.asyncio
+async def test_boilerplate() -> None:
+    import uuid
+    import time
     
+    async def some_service() -> uuid.UUID:
+        await asyncio.sleep(1)
+        return uuid.uuid4()
+
+    async def  main() -> Tuple[uuid.UUID, uuid.UUID]:
+        _id: uuid.UUID = await some_service()
+        other_id: uuid.UUID = await some_service()
+        return (_id, other_id)
+
+    @dependant
+    async def main_with_depends(
+        *, _id: uuid.UUID = Depends(some_service), other_id: uuid.UUID = Depends(some_service),
+    ) -> Tuple[uuid.UUID, uuid.UUID]:
+        return (_id, other_id)
+
+    pt = time.process_time()
+    t = time.time()
+    await main()
+    elapsed_pt = (time.process_time() - pt)
+    elapsed_t = (time.time() - t)
+    print(f'process: {elapsed_pt}, time: {elapsed_t}')
+
+    pt = time.process_time()
+    t = time.time()
+    await main_with_depends()
+    elapsed_pt = (time.process_time() - pt)
+    elapsed_t = (time.time() - t)
+    print(f'process: {elapsed_pt}, time: {elapsed_t}')
