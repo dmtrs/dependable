563fea96513ecf8b08a4d06227e810ce4b2dc1cb
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2021-03-31 00:09:41 +0300

    fix(tests): Fixs lint error

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,13 +1,14 @@
 import asyncio
-import pytest
 from typing import Dict, Tuple
-from collections import defaultdict
 
-from dependable import dependant, Depends
+import pytest
+
+from dependable import Depends, dependant
+
 
 @pytest.mark.asyncio
 async def test_gotcha() -> None:
-    async def foo(r: Dict[str, str] = {} ) -> Dict[str, str]:
+    async def foo(r: Dict[str, str] = {}) -> Dict[str, str]:
         return r
 
     assert await foo() is await foo() is not {}
@@ -38,34 +39,36 @@ async def test_awaitable() -> None:
 
 @pytest.mark.asyncio
 async def test_boilerplate() -> None:
-    import uuid
     import time
-    
+    import uuid
+
     async def some_service() -> uuid.UUID:
         await asyncio.sleep(1)
         return uuid.uuid4()
 
-    async def  main() -> Tuple[uuid.UUID, uuid.UUID]:
+    async def main() -> Tuple[uuid.UUID, uuid.UUID]:
         _id: uuid.UUID = await some_service()
         other_id: uuid.UUID = await some_service()
         return (_id, other_id)
 
     @dependant
     async def main_with_depends(
-        *, _id: uuid.UUID = Depends(some_service), other_id: uuid.UUID = Depends(some_service),
+        *,
+        _id: uuid.UUID = Depends(some_service),
+        other_id: uuid.UUID = Depends(some_service),
     ) -> Tuple[uuid.UUID, uuid.UUID]:
         return (_id, other_id)
 
     pt = time.process_time()
     t = time.time()
     await main()
-    elapsed_pt = (time.process_time() - pt)
-    elapsed_t = (time.time() - t)
-    print(f'process: {elapsed_pt}, time: {elapsed_t}')
+    elapsed_pt = time.process_time() - pt
+    elapsed_t = time.time() - t
+    print(f"process: {elapsed_pt}, time: {elapsed_t}")
 
     pt = time.process_time()
     t = time.time()
     await main_with_depends()
-    elapsed_pt = (time.process_time() - pt)
-    elapsed_t = (time.time() - t)
-    print(f'process: {elapsed_pt}, time: {elapsed_t}')
+    elapsed_pt = time.process_time() - pt
+    elapsed_t = time.time() - t
+    print(f"process: {elapsed_pt}, time: {elapsed_t}")
